b3607797db0ede6f65e555282858f006174d8cca
kamalprassath30 <kamalprasath2003@gmail.com>
2025-01-26 19:51:31 +0530

    Gemini Clone project

---
+++ src/config/gemini.js
@@ -0,0 +1,36 @@
+const apiKey = "AIzaSyCZu5CAuRs0ne4X51p6mzLrSwuf6Gze_nM";
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+// const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response.text();
+  console.log(response);
+  return response;
+}
+
+export default run;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-AipHxkoLHORoLaTNypGtEsVjIuCbXUQ",
+  authDomain: "gemini-29316.firebaseapp.com",
+  projectId: "gemini-29316",
+  storageBucket: "gemini-29316.firebasestorage.app",
+  messagingSenderId: "65959634264",
+  appId: "1:65959634264:web:4ed2e00c7691188ea3b12d",
+};
